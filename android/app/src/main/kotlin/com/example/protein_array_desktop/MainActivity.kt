c93b445bec6aa55d01db8ea5fbeab5a95ed4f869
low0621 <low0621@gmail.com>
2022-10-14 16:49:43 +0800

    kick off

---
+++ android/app/src/main/kotlin/com/example/protein_array_desktop/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.protein_array_desktop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ main.py
@@ -0,0 +1,4 @@
+import sys
+
+with open(f"{sys.argv[1]}/test.txt", "w") as f:
+  f.write("Hello")
